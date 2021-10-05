cefcd38396dcac273cac56eff5f9a7271f66b2bb
FernandoF <c14170084@john.petra.ac.id>
2021-10-06 02:26:41 +0700

    BR-4/Get-All - Function Get All Done

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -70,3 +70,14 @@ exports.delete = (req,res) => {
         })
     })
 }
+
+exports.findAll = (req, res) => {
+    Post.find()
+    .then((result) => {
+        res.send(result)
+    }).catch((err) => {
+        res.status(500).send({
+            message: err.message || "Some error while retrieving posts."
+        })
+    });
+} 
--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -5,5 +5,6 @@ module.exports = (app) => {
     router.post('/', posts.create)
     router.put('/:id', posts.update)
     router.delete('/:id', posts.delete)
+    router.get('/', posts.findAll)
     app.use('/api/posts', router)
 }
